43cee77333ddb574a51447f8f07e0f8dc722485d
max05051998 <maxsteiger98@gmail.com>
2016-03-23 14:42:56 +0100

    Started to implement search function

---
+++ app/src/main/java/ch/company/ameno/meme_sounds/SearchableActivity.java
@@ -0,0 +1,24 @@
+package ch.company.ameno.meme_sounds;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+//public class SearchableActivity {
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.search);
+//
+//        // Get the intent, verify the action and get the query
+//        Intent intent = getIntent();
+//        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+//            String query = intent.getStringExtra(SearchManager.QUERY);
+//            doMySearch(query);
+//        }
+//    }
+//    public static String doMySearch(){
+//
+//        return
+//    }
+//}
